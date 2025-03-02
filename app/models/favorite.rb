56d2bb918a425f23384b31b09f0d7dc54667a3d3
seki221 <pawls00.bugaboo@icloud.com>
2025-03-02 21:05:17 +0900

    update rename

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -4,5 +4,5 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :schedule
 
-  validates :user_id, uniqueness: { scope: :scheduled_id }
+  validates :user_id, uniqueness: { scope: :schedule_id }
 end
